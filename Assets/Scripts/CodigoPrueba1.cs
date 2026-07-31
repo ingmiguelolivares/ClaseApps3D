e397baabb20d4c9d09d4dd37c2ea9aac879f353f
ingmiguelolivares <miguelolivares1981@gmail.com>
2026-07-31 14:32:44 -0500

    Commit de muestra
    se creo un codigo para mostrar ejemplo de como hacer commit y push posteriormente

---
+++ Assets/Scripts/CodigoPrueba1.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CodigoPrueba1 : MonoBehaviour
+{
+    //Desarrollado por miguel Olivares
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
